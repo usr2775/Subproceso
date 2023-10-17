f675631929f70eff94cd71be71d8fcbbc0cad413
Chibuike <chibuike.dmn@gmail.com>
2023-10-17 15:51:38 +0100

    feat: creado de funciones de clase VowelCounter

--- src/net/salesianos/processes/VowelCounter.java
+++ src/net/salesianos/processes/VowelCounter.java
@@ -7,5 +7,22 @@ public class VowelCounter {
             System.exit(1);
         }
 
-}
+        String text = args[0];
+        int vowelCount = countVowels(text);
+        System.out.println(vowelCount);
+    }
+
+    private static int countVowels(String text) {
+        int count = 0;
+        for (char c : text.toCharArray()) {
+            if (isVowel(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean isVowel(char c) {
+        return "aeiou".indexOf(c) != -1;
+    }
 }
