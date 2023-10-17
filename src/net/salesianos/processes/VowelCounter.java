e9d1370d4ec42e89af473df66457816d57527008
Chibuike <chibuike.dmn@gmail.com>
2023-10-17 15:48:07 +0100

    feat: creado clase VowelCounter y verificacion

---
+++ src/net/salesianos/processes/VowelCounter.java
@@ -0,0 +1,11 @@
+package net.salesianos.processes;
+
+public class VowelCounter {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Uso: VowelCounter <texto>");
+            System.exit(1);
+        }
+
+}
+}
